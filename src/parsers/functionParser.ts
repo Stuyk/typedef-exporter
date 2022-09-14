819c36d956f829b087ea9e61ea92a4b3bd955d73
stuyk <stuykgaming@gmail.com>
2022-09-13 19:22:32 -0600

    Reduce Complexity

--- src/parsers/functionParser.ts
+++ src/parsers/functionParser.ts
@@ -23,38 +23,40 @@ export function getFunctions(contents: string[]): Array<FunctionInfo> {
         if (isFindingComment && line.includes('*')) {
             currentComments.push(line);
 
-            // Means it ends now...
-            if (isFindingComment && line.includes('/**')) {
-                isFindingComment = false;
-
-                if (currentComments.length <= 1) {
-                    functions[functions.length - 1].comments = currentComments.join('\r\n');
-                    continue;
-                }
-
-                // Hard to read this; but basicaly...
-                // Read the function params from JSDoc styling
-                // Loop through the params, and replace the 'key' if it is present in the string.
-                // Once the 'key' is removed. It should be safe to append the comment to the function args.
-                const commentInfo = commentParser(currentComments);
-                for (let paramIndex = 0; paramIndex < commentInfo.params.length; paramIndex++) {
-                    if (functions[functions.length - 1].arguments[paramIndex]) {
-                        const key = functions[functions.length - 1].arguments[paramIndex].key;
-                        const keyLength = key.replace('...', '').length + 1;
-                        const currentComment = commentInfo.params[paramIndex];
-                        const cleanedComment = currentComment.slice(keyLength, currentComment.length);
-                        functions[functions.length - 1].arguments[paramIndex].comment = cleanedComment;
-                    }
-                }
-
-                functions[functions.length - 1].comments = commentInfo.descriptions.join('\r\n');
-                if (commentInfo.isDeprecated) {
-                    functions[functions.length - 1].isDeprecated = true;
-                }
-
-                currentComments = [];
+            // There are more things to process.
+            if (!line.includes('/**')) {
+                continue;
             }
 
+            // There is nothing left to process.
+            isFindingComment = false;
+
+            if (currentComments.length <= 1) {
+                functions[functions.length - 1].comments = currentComments.join('\r\n');
+                continue;
+            }
+
+            // Hard to read this; but basicaly...
+            // Read the function params from JSDoc styling
+            // Loop through the params, and replace the 'key' if it is present in the string.
+            // Once the 'key' is removed. It should be safe to append the comment to the function args.
+            const commentInfo = commentParser(currentComments);
+            for (let paramIndex = 0; paramIndex < commentInfo.params.length; paramIndex++) {
+                if (functions[functions.length - 1].arguments[paramIndex]) {
+                    const key = functions[functions.length - 1].arguments[paramIndex].key;
+                    const keyLength = key.replace('...', '').length + 1;
+                    const currentComment = commentInfo.params[paramIndex];
+                    const cleanedComment = currentComment.slice(keyLength, currentComment.length);
+                    functions[functions.length - 1].arguments[paramIndex].comment = cleanedComment;
+                }
+            }
+
+            functions[functions.length - 1].comments = commentInfo.descriptions.join('\r\n');
+            if (commentInfo.isDeprecated) {
+                functions[functions.length - 1].isDeprecated = true;
+            }
+
+            currentComments = [];
             continue;
         }
 
